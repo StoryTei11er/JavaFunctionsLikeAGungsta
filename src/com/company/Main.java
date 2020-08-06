b184fe40106153ca97694550533a5b4eda84b40e
StoryTei11er <sitnyanskij.aleksandr@gmail.com>
2020-08-06 21:55:35 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+//        Supplier<String> suppliedValue = () -> "one";
+//        Supplier<List<String>> suppliedArrayList = ArrayList::new;
+
+//        List<String> listOfElement = new ArrayList<>(Arrays.asList("1","2", "3", "4" ,"5"));
+//        BinaryOperator<String> binaryOperator = (sb1, sb2) -> sb1 +"_________" + (sb2) ;
+
+
+////        List<String> neededList =  MyStreamAPI.generate(suppliedValue, suppliedArrayList, 2);
+////        Optional<String> count = MyStreamAPI.reduce(integerList, binaryOperator);
+        
+
+
+    }
+
+}
+
---
+++ src/com/company/MyStreamAPI.java
@@ -0,0 +1,97 @@
+package com.company;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class MyStreamAPI {
+
+    public static <E> List<E> generate(Supplier<E> generator,
+                                       Supplier<List<E>> listFactory,
+                                       int count) {
+
+        List<E> temporaryList = listFactory.get();
+
+        int i = 0;
+        do {
+            temporaryList.add((E) generator.get());
+            i++;
+        } while (i != count);
+
+        return temporaryList;
+    }
+
+    public static <E> Optional<E> reduce(List<E> elements, BinaryOperator<E> accumulator) {
+        List<E> temporaryList = new ArrayList<>();
+
+        int neededIndex = elements.size() / 2;
+
+        if (elements.size() % 2 == 0)
+            neededIndex--;
+
+        E intermediateElement = accumulator.apply(elements.get(0), elements.get(1));
+        for (int i = 2; i < elements.size(); i++) {
+            intermediateElement = accumulator.apply(intermediateElement, elements.get(i));
+            temporaryList.add(intermediateElement);
+        }
+
+        Optional<E> result = Optional.of(temporaryList.get(neededIndex));
+        return result;
+    }
+
+    public static <E> E reduce(E seed, List<E> elements, BinaryOperator<E> accumulator) {
+        //TODO Implement me
+        return null;
+    }
+
+    public static <E> Map<Boolean, List<E>> partitionBy(List<E> elements,
+                                                        Predicate<E> predicate,
+                                                        Supplier<Map<Boolean, List<E>>> mapFactory,
+                                                        Supplier<List<E>> listFactory) {
+        //TODO Implement me
+        return null;
+    }
+
+    public static <T, K> Map<K, List<T>> groupBy(List<T> elements,
+                                                 Function<T, K> classifier,
+                                                 Supplier<Map<K, List<T>>> mapFactory,
+                                                 Supplier<List<T>> listFactory) {
+        //TODO Implement me
+        return null;
+    }
+
+    public static <T, K, U> Map<K, U> toMap(List<T> elements,
+                                            Function<T, K> keyFunction,
+                                            Function<T, U> valueFunction,
+                                            BinaryOperator<U> mergeFunction,
+                                            Supplier<Map<K, U>> mapFactory) {
+        //TODO Implement me
+        return null;
+    }
+
+    //ADDITIONAL METHODS
+
+    public static <E, T> Map<Boolean, List<T>> partitionByAndMapElement(List<E> elements,
+                                                                        Predicate<E> predicate,
+                                                                        Supplier<Map<Boolean, List<T>>> mapFactory,
+                                                                        Supplier<List<T>> listFactory,
+                                                                        Function<E, T> elementMapper) {
+        //TODO Implement me
+        return null;
+    }
+
+    public static <T, U, K> Map<K, List<U>> groupByAndMapElement(List<T> elements,
+                                                                 Function<T, K> classifier,
+                                                                 Supplier<Map<K, List<U>>> mapFactory,
+                                                                 Supplier<List<U>> listFactory,
+                                                                 Function<T, U> elementMapper) {
+        //TODO Implement me
+        return null;
+    }
+
+}
+
+
+
