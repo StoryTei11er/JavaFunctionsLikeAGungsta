392c26ec0f43499a66d882b0014f82fa76e7fcd6
StoryTei11er <sitnyanskij.aleksandr@gmail.com>
2020-08-07 18:42:33 +0300

    A lot of hard job)))

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,11 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -14,18 +14,44 @@ public class Main {
 
 
 //        Supplier<String> suppliedValue = () -> "one";
-//        Supplier<List<String>> suppliedArrayList = ArrayList::new;
+        Supplier<List<String>> suppliedStringArrayList = ArrayList::new;
+//        List<String> resultOfSupply = MyStreamAPI.generate(suppliedValue, suppliedStringArrayList, 2);
+//
+//        List<String> listOfElement = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5"));
+//        BinaryOperator<String> binaryOperator = (sb1, sb2) -> sb1 + "_________" + (sb2);
+//        Optional<String> finalViewOfLit = MyStreamAPI.reduce(listOfElement, binaryOperator);
+//
+//        String seed = "startOfArray";
+//        String listWithSeed = MyStreamAPI.reduce(seed, listOfElement, binaryOperator);
+//
+//        Consumer<String> consumer = s -> System.out.println("I am consumer, I can`t return anything");
+//
+//        List<String> notDistinctList = new ArrayList<String>(Arrays.asList("1", "1", "1", "1", "1", "2", "3"));
+//        List<String> distinctList = MyStreamAPI.distinct(notDistinctList, suppliedStringArrayList);
+//
+//        List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+//        Optional<Integer> minimumValueOfList = MyStreamAPI.min(integerList, Integer::compareTo);
+//
+//        Optional<Integer> maximumValueOfList = MyStreamAPI.max(integerList, Integer::compareTo);
+//
+//        List<Integer> unFilteredList = new ArrayList<>(Arrays.asList(5, 3, 2, 1, 6, 7, 8, 9, 4));
+//        List<Integer> filteredList = MyStreamAPI.filter(unFilteredList, element -> element < 7);
+//
+//        boolean isAnyMatch = MyStreamAPI.anyMatch(unFilteredList, element -> element > 622);
+//
+//        boolean isAllMatch = MyStreamAPI.allMatch(unFilteredList, element -> element < 9);
+//
+//        boolean isNoneMatch = MyStreamAPI.noneMatch(unFilteredList, element -> element <= 9);
+//
+//        List<String> primaryList = new ArrayList<>(Arrays.asList("1", "2", "3"));
+//        Function<String, Integer> mappingFunction = s -> new Integer(Integer.valueOf(s));
+//        Supplier<List<Integer>> suppliedIntegerArrayList = ArrayList::new;
+//        List<Integer> listAfterMapping = MyStreamAPI.map(primaryList, mappingFunction, suppliedIntegerArrayList);
 
-        List<String> listOfElement = new ArrayList<>(Arrays.asList("1","2", "3", "4" ,"5"));
-        BinaryOperator<String> binaryOperator = (sb1, sb2) -> sb1 +"_________" + (sb2) ;
-
-
-////        List<String> neededList =  MyStreamAPI.generate(suppliedValue, suppliedArrayList, 2);
-        Optional<String> count = MyStreamAPI.reduce(listOfElement, binaryOperator);
-        System.out.println(count);
-
-
-
+//        String predicateValue = "Sasha";
+//        Supplier<Map<Boolean, List<String>>> suppliedMap = HashMap::new;
+//        List<String> listOfStrings = new ArrayList<>(Arrays.asList("Vova", "Sasha"));
+//        Map<Boolean,List<String>> partitionedMap = MyStreamAPI.partitionBy(listOfStrings, element -> element.equals(predicateValue), suppliedMap, suppliedStringArrayList);
 
     }
 
--- src/com/company/MyStreamAPI.java
+++ src/com/company/MyStreamAPI.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import java.util.*;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class MyStreamAPI {
 
@@ -14,38 +11,110 @@ public class MyStreamAPI {
 
         List<E> temporaryList = listFactory.get();
 
-        int i = 0;
-        do {
+        for (int i = 0; i < count; i++) {
             temporaryList.add((E) generator.get());
-            i++;
-        } while (i != count);
-
+        }
         return temporaryList;
     }
 
     public static <E> Optional<E> reduce(List<E> elements, BinaryOperator<E> accumulator) {
 
-        E intermediateElement = accumulator.apply(elements.get(0), elements.get(1));
+        E resultOfReduce = accumulator.apply(elements.get(0), elements.get(1));
         for (int i = 2; i < elements.size(); i++) {
-            intermediateElement = accumulator.apply(intermediateElement, elements.get(i));
+            resultOfReduce = accumulator.apply(resultOfReduce, elements.get(i));
         }
-
-        Optional<E> result = Optional.of(intermediateElement);
-
-        return result;
+        return Optional.of(resultOfReduce);
     }
 
     public static <E> E reduce(E seed, List<E> elements, BinaryOperator<E> accumulator) {
-        //TODO Implement me
-        return null;
+        E resultOfReduce = seed;
+        for (E element : elements) {
+            resultOfReduce = accumulator.apply(resultOfReduce, element);
+        }
+        return resultOfReduce;
     }
+
+    public static <E> void forEach(List<E> elements, Consumer<E> consumer) {
+        for (E element : elements) {
+            consumer.accept(element);
+        }
+    }
+
+    public static <E> List<E> distinct(List<E> elements, Supplier<List<E>> listFactory) {
+        List<E> listOfDistinctValue = listFactory.get();
+        Set<E> set = new HashSet<>(elements);
+        listOfDistinctValue.addAll(set);
+        return listOfDistinctValue;
+    }
+
+    public static <E> Optional<E> min(List<E> elements, Comparator<E> comparator) {
+        elements.sort(comparator);
+        return Optional.of(elements.get(0));
+    }
+
+    public static <E> Optional<E> max(List<E> elements, Comparator<E> comparator) {
+        int maxValue = elements.size() - 1;
+        elements.sort(comparator);
+        return Optional.of(elements.get(maxValue));
+    }
+
+    public static <E> List<E> filter(List<E> elements, Predicate<E> filter) {
+        List<E> filteredList = new ArrayList<>();
+        for (E element : elements) {
+            if (filter.test(element))
+                filteredList.add(element);
+        }
+        return filteredList;
+    }
+
+    public static <E> boolean anyMatch(List<E> elements, Predicate<E> predicate) {
+        for (E element : elements)
+            if (predicate.test(element))
+                return true;
+        return false;
+    }
+
+    public static <E> boolean allMatch(List<E> elements, Predicate<E> predicate) {
+        for (E element : elements)
+            if (!predicate.test(element))
+                return false;
+        return true;
+    }
+
+    public static <E> boolean noneMatch(List<E> elements, Predicate<E> predicate) {
+        for (E element : elements)
+            if (!predicate.test(element))
+                return true;
+        return false;
+    }
+
+    public static <T, R> List<R> map(List<T> elements,
+                                     Function<T, R> mappingFunction,
+                                     Supplier<List<R>> listFactory) {
+        List<R> result = listFactory.get();
+        for (T element : elements)
+            result.add(mappingFunction.apply(element));
+        return result;
+    }
+
 
     public static <E> Map<Boolean, List<E>> partitionBy(List<E> elements,
                                                         Predicate<E> predicate,
                                                         Supplier<Map<Boolean, List<E>>> mapFactory,
                                                         Supplier<List<E>> listFactory) {
-        //TODO Implement me
-        return null;
+        List<E> valueOfTrueKey = listFactory.get();
+        List<E> valueOfFalseKey = listFactory.get();
+
+        for (E element : elements)
+            if (predicate.test(element))
+                valueOfTrueKey.add(element);
+            else valueOfFalseKey.add(element);
+
+        Map<Boolean, List<E>> result = mapFactory.get();
+            result.put(true, valueOfTrueKey);
+            result.put(false, valueOfFalseKey);
+
+        return result;
     }
 
     public static <T, K> Map<K, List<T>> groupBy(List<T> elements,
@@ -65,7 +134,6 @@ public class MyStreamAPI {
         return null;
     }
 
-    //ADDITIONAL METHODS
 
     public static <E, T> Map<Boolean, List<T>> partitionByAndMapElement(List<E> elements,
                                                                         Predicate<E> predicate,
@@ -85,7 +153,10 @@ public class MyStreamAPI {
         return null;
     }
 
+
 }
+
+
 
 
 
